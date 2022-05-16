e14b0131bcd838ae07ed5b2af45a7c02b48da7fb
Rytis <rytis.cesna@gmail.com>
2022-05-16 13:48:08 +0300

    Cert5 version 03

--- cert/cert5_cash_register.js
+++ cert/cert5_cash_register.js
@@ -1,73 +1,44 @@
-function checkCashRegister(price, cash, cid) {
+function checkCashRegister(price, cash, cid) {//Using values x100(in cents) to avoid float calculations 
+  const values = {"PENNY": 1, "NICKEL": 2, "DIME": 10, "QUARTER": 25, "ONE": 100,
+                  "FIVE": 500, "TEN": 1000, "TWENTY": 2000, "ONE HUNDRED": 10000}; 
   
   let filtered = cid.filter(elem => elem[1] !== 0).reverse();
-  let kasoje = [];
-  
-  kasoje  = filtered.map((elem) => { //Avoiding float calculation shenanigans
-    kasoje.push(elem[0])
-    kasoje.push(Math.round(elem[1]*100))
-    return kasoje
-  });    
-  let out = {status: "INSUFFICIENT_FUNDS", change: []};//Default return
-
-
-  //console.log(kasoje)
-
-
-  
-  let graza = Math.round(cash*100) - Math.round(price*100) //Avoiding float. Calculating in cents.
-  
-  //let kasoje = {};//cashOnHand
   let grazinti = {};
 
-  cid.forEach((value) => {kasoje[value[0]] = Math.round(value [1]*100)})
-  return out
+  let kasoje = {};
+    
+  for (let item  of filtered){
+    kasoje[item[0]] = Math.round(item[1]*100)//Calculatiing in cents
+  };
+    
+  const VISO = Object.values(kasoje).reduce((accumulator, curr) => accumulator + curr)
+    console.log(VISO);
+    
+  let graza = Math.round(cash*100) - Math.round(price*100) //Avoiding float. Calculating in cents.
+  console.log(graza);
+    
+  if (VISO - graza < 0){
+    let out = {status: "INSUFFICIENT_FUNDS", change: []};
+    return out
+  }
+  const pinVardas = Object.keys(kasoje)//Getting all Oject.kasoje keys(money names)
+  let index = 0;
+
+  while ( index < pinVardas.length && graza > 0 ){
+    let pinigas  = values[pinVardas[index]];
+    if (pinigas <= graza ){
+      
+      console.log(pinigas)
+    }
+    
+    index++
+  }
+
+
+  //return out
 }
 
-let result = checkCashRegister(19.5, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1],
+let result = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1],
  ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
    console.log(result)
 
-
-/*   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1],
- ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
-*/
-  /*let i = VALUES.length - 1; 
-  while(i >= 0 && graza > 0) {
-    let verte = VALUES[i];
-    
-    if( graza - verte[1] > 0 && kasoje[verte[0]], graza){
-      grazinti[verte[0]] = 0;
-      
-      while(kasoje[verte[0]] > 0 && graza - verte[1] >= 0){
-        kasoje[verte[0]] -= verte[1];
-        graza -= verte[1];
-        grazinti[verte[0]] += verte[1];
-      }
-    }
-    i--; //Frik'n while loop indexer!  
-  }
-
-  if (graza === 0){
-    let kasaTuscia = true;
-     Object.keys(kasoje).forEach((pinigas) =>{//Checking if there are money in the register
-      if (kasoje[pinigas] > 0) {
-        kasaTuscia = false;
-      };
-    });
-    
-    if (kasaTuscia){
-      out = {status: "CLOSED", change: cid};
-      console.log(out);
-      return out;
-    } else{
-      let temp = [];
-      Object.keys(grazinti).map((moneta) =>{
-        if (grazinti[moneta] > 0){
-          temp.push([moneta, grazinti[moneta] / 100]);//Getting back to dollars from cents.
-      }
-    });
-    out = {status: "OPEN", change: temp};
-    return out
-    }
-  }*/
